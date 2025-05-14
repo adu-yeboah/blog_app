8ac5876e882e89de0c03ca3e1f182e7c192cf1b4
Ace <ace.dhe.general@gmail.com>
2025-05-13 23:37:40 -0700

    models

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
+// inertia::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get("/", function () {
+    return Inertia::render("main/Home", [
+        "message" => "hello",
+    ]);
+});
+
+// message routes
+Route::prefix("message")->group(function () {
+    Route::get("/", [MessageController::class, "index"])->name("message.index");
+    Route::post("/create", [MessageController::class, "store"])->name("message.store");
+    Route::get("/{id}", [MessageController::class, "show"])->name("message.show");
+    Route::put("/{id}", [MessageController::class, "update"])->name("message.update");
+    Route::delete("/{id}", [MessageController::class, "destory"])->name("blog.store");
+});
+
+// blog routes
+Route::prefix("blog")->group(function () {
+    Route::get("/", [BlogController::class, "index"])->name("blog.index");
+    Route::post("/create", [BlogController::class, "store"])->name("blog.store");
+    Route::get("/{id}", [BlogController::class, "show"])->name("blog.show");
+    Route::put("/{id}", [BlogController::class, "update"])->name("blog.update");
+    Route::delete("/{id}", [BlogController::class, "destory"])->name("blog.store");
 });
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-
+import { resolve } from 'path'
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'resources/js'),
+    },
+  },
 });
